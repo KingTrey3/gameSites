92293bcee5b4b1e15a5f68dfb2ae3a4984b76609
trey03 <adixon1@unc.edu>
2024-05-24 16:39:29 -0400

    initial commit, routing for sudoku has been established

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SudokuComponent } from './sudoku/sudoku.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'sudoku',
+        component: SudokuComponent
+    }
+];
---
+++ src/app/sudoku/sudoku.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sudoku',
+  standalone: true,
+  imports: [],
+  templateUrl: './sudoku.component.html',
+  styleUrl: './sudoku.component.scss'
+})
+export class SudokuComponent {
+
+}
