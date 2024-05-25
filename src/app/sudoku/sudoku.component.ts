77fcaa64b5ea2a133acc7987aef9e48950501533
trey03 <adixon1@unc.edu>
2024-05-25 02:49:06 -0400

    takes user input and creates array for ts file

--- src/app/sudoku/sudoku.component.ts
+++ src/app/sudoku/sudoku.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sudoku',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './sudoku.component.html',
   styleUrl: './sudoku.component.scss'
 })
 export class SudokuComponent {
+  sudokuData: number[][] = [];
 
+  constructor() {
+    this.initializeSudokuData();
+  }
+
+
+  initializeSudokuData() {
+    // Initialize sudokuData with null values (user inputs will replace these)
+    for (let i = 0; i < 9; i++) {
+      this.sudokuData[i] = [];
+      for (let j = 0; j < 9; j++) {
+        this.sudokuData[i][j] = 0; // Initial value is null
+      }
+    }
+  }
+  submitSudoku() {
+    console.log(this.sudokuData);
+  }
 }
