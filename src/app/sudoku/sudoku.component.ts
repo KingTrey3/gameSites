e885d4d438ebcab0a2ce0b5156f1fe9c6b728cde
trey03 <adixon1@unc.edu>
2024-05-26 17:11:26 -0400

    finished sudoku checker that works

--- src/app/sudoku/sudoku.component.ts
+++ src/app/sudoku/sudoku.component.ts
@@ -28,6 +28,7 @@ export class SudokuComponent {
   submitSudoku() {
     console.log(this.sudokuData);
     console.log(this.completeBoard());
+    console.log(this.isValidSudoku());
   }
 
   completeBoard(): boolean {
@@ -42,4 +43,66 @@ export class SudokuComponent {
       return true;
   }
 
+  isValidSudoku(): boolean {
+    var N = 9;
+
+    if (this.completeBoard() == false) {
+      return false;
+    }
+
+    var unique = Array(N + 1).fill(false);
+
+    //rows
+    for(var i = 0; i < N; i++) {
+      unique = Array(N+1).fill(false);
+
+      for(var j = 0; j < N; j++) {
+        var z = this.sudokuData[i][j];
+
+        if (unique[z]) {
+          return false;
+        }
+        unique[z] = true;
+      }
+    }
+    console.log("rows are good")
+    //colums
+    for (var i = 0; i < N; i++) {
+      unique = Array(N+1).fill(false);
+
+      for (var j = 0; j < N; j++) {
+        var z = this.sudokuData[j][i];
+
+        if(unique[z]) {
+          return false;
+        }
+        unique[z] = true;
+      }
+    }
+    console.log("cols are good")
+    // 3 * 3 blocks
+    for (var i = 0; i < N - 2; i += 3) {
+      for (var j = 0; j < N - 2; j+= 3) {
+
+        unique = Array(N+1).fill(false);
+
+        for(var k = 0; k < 3; k++) {
+          for(var l = 0; l < 3; l++) {
+
+            var X = i + k;
+
+            var Y = j + l;
+
+            var Z = this.sudokuData[X][Y];
+
+            if (unique[Z]) {
+              return false;
+            }
+            unique[Z] = true;
+          }
+        }
+      }
+    }
+    return true;
+  }
 }
